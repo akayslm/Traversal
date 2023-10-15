99cf450aac62e26a837f145c9eac272b12f43b56
akayslm <73195655+akayslm@users.noreply.github.com>
2023-10-15 23:16:26 +0300

    Testimonial Listesi ayarlandı

--- Traversal/Traversal/ViewComponents/Default/_TestimonialsPartial.cs
+++ Traversal/Traversal/ViewComponents/Default/_TestimonialsPartial.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace Traversal.ViewComponents.Default
 {
     public class _TestimonialsPartial : ViewComponent
     {
+        TestimonialManager testimonialManager = new TestimonialManager(new EfTestimonialDal());
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = testimonialManager.TGetList();
+            return View(values);
         }
     }
 }
